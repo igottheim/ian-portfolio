42f6a2f197b196df3311e9b0eaf0e275152cad7a
ian gottheim <iangottheim@gmail.com>
2023-01-11 14:03:49 -0500

    fix temp deployment

--- src/Views/Projects.jsx
+++ src/Views/Projects.jsx
@@ -3,12 +3,19 @@ import { useState } from 'react'
 import'../assets/css/index.css';
 
 import { Breadcrumbs } from "react-daisyui";
+import Button from 'react-daisyui';
 
 function Projects() {
   const [count, setCount] = useState(0)
 
   return (
-   <div>Hello Ian</div>
+    <>
+   <div>Portfolio For Ian Gottheim in Progress</div>
+   <Button href = "https://www.linkedin.com/in/ian-gottheim/">Click for LinkedIn</Button>
+   <Button href = "https://github.com/igottheim">Click for Github</Button>
+   <div>Phone: 201-906-2950</div>
+   <div>Email: iangottheim@gmail.com</div>
+   </>
   )
 }
 
