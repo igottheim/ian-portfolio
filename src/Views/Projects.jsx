bd7b2657f636b9bc61f2db1023f09ff2115f3a43
ian gottheim <iangottheim@gmail.com>
2023-01-12 11:08:20 -0500

    fixes to temp portfolio

--- src/Views/Projects.jsx
+++ src/Views/Projects.jsx
@@ -11,12 +11,22 @@ function Projects() {
 
   return (
     <>
+    <div>
    <div>Portfolio For Ian Gottheim in Progress</div>
    <a href = "https://www.linkedin.com/in/ian-gottheim/">Click for LinkedIn</a>
    <div></div>
    <a href = "https://github.com/igottheim">Click for Github</a>
    <div>Phone: 201-906-2950</div>
    <div>Email: iangottheim@gmail.com</div>
+   </div>
+   <div>Check out Some Amazing Project I Worked On!
+   <div>
+    <a href = "https://flatironopensource.ml/">Flatiron Open Source</a>
+    </div>
+    <div>
+    <a href = "https://taskforcev2.onrender.com/">Flatiron Miscord</a>
+    </div>
+   </div>
 
    </>
   )
